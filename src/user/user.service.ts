58ce36a38222bf5feebbdc35b18d33a6b8bbb810
heewoong-ahn <dksgmldnd@korea.ac.kr>
2024-03-27 21:53:44 +0900

    ftr: user GET, POST, DELETE

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,4 +7,23 @@ export class UserService {
   getUsers(): string[] {
     return this.users;
   }
+
+  async addUser(name: string): Promise<string[]> {
+    if (!this.users.includes(name)) {
+      await this.users.push(name);
+    }
+    return this.users;
+  }
+
+  async deleteUser(name: string): Promise<string[]> {
+    if (this.users.includes(name)) {
+      await this.users.splice(this.users.indexOf(name), 1);
+      // 1이 의미하는 것은 1개를 지운다.
+    }
+    return this.users;
+  }
+
+  getUserName(index: number): string {
+    return this.users[index];
+  }
 }
